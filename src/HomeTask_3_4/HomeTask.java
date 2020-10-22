d46386fb0b3f7b261467debcecddd56b0fb29056
Melnyk_Volodymyr <olgreen123@gmail.com>
2020-10-22 20:21:58 +0300

    Work with git

--- src/HomeTask_3_4/HomeTask.java
+++ src/HomeTask_3_4/HomeTask.java
@@ -1,8 +1,16 @@
 package HomeTask_3_4;
 
+import java.util.Scanner;
+
 public class HomeTask {
 
-    public static void mein(String[] args) {
+    public static void main(String[] args) {
+        String Text = "Розв'язків рівняння не має";
+        System.out.println("Розв'язання квадратного рівняння: ax^2+bx+c=0");
+        System.out.println("Введіь будь яке ціле число: a" );
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+
 
 
 
--- src/com/Main.java
+++ src/com/Main.java
@@ -1,14 +1,1 @@
-package com;
 
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Розв'язання квадратного рівняння: ax^2+bx+c=0");
-        System.out.println("Введіть будь яке ціле число: a ");
-
-
-
-
-    }
-}
