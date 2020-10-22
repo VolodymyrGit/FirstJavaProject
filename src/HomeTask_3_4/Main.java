b98a5935bc2d7d4e6630928c61e7b1a3501839e6
Melnyk_Volodymyr <olgreen123@gmail.com>
2020-10-22 07:41:14 +0300

    Initial message

---
+++ src/HomeTask_3_4/Main.java
@@ -0,0 +1,4 @@
+package HomeTask_3_4;
+
+public class Main {
+}
--- src/com/Main.java
+++ src/com/Main.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Розв'язання квадратного рівняння: ax^2+bx+c=0");
+        System.out.println("Введіть будь яке ціле число: a ");
 
 
 
