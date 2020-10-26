b0c3b49b33b17c7078873a3623e872358df2b472
Melnyk_Volodymyr <olgreen123@gmail.com>
2020-10-26 08:52:12 +0200

    Поки працює

--- src/HomeTask_3_4/HomeTask.java
+++ src/HomeTask_3_4/HomeTask.java
@@ -5,15 +5,20 @@ import java.util.Scanner;
 public class HomeTask {
 
     public static void main(String[] args) {
-        String Text = "Розв'язків рівняння не має";
+        String end = "Розв'язків рівняння не має";
         System.out.println("Розв'язання квадратного рівняння: ax^2+bx+c=0");
-        System.out.println("Введіь будь яке ціле число: a" );
+        System.out.print("Введіь будь яке ціле число: a " );
         Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
-
-
-
-
-
+        double a = scan.nextInt();
+        System.out.print("Введіь будь яке ціле число: b " );
+        double b = scan.nextInt();
+        System.out.print("Введіь будь яке ціле число: c " );
+        double c = scan.nextInt();
+        if (a == 0 && b == 0)
+        System.out.println("Змінні a=0, b=0, тому " + end);
+        else if (a == 0 && b != 0) {
+            System.out.println("Квадратне рівняння перетворюється в лінійне: bx+c=0");
+            System.out.println("x = " + (-c / b));
+        }
     }
 }
