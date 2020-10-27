0dca8faba001b0436203e0a9b0e269569a80f174
Melnyk_Volodymyr <olgreen123@gmail.com>
2020-10-27 07:06:13 +0200

    One more task)

---
+++ src/HomeTask_3_4/Sequence.java
@@ -0,0 +1,15 @@
+package HomeTask_3_4;
+
+public class Sequence {
+
+    public static void main(String[] args) {
+
+        int a = -1;
+        int i = 1;
+        while (i <= 55) {
+            i ++;
+            a += 2;
+            System.out.println(a);
+        }
+    }
+}
