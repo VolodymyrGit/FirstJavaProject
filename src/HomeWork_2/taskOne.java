210073e4d125b52a3af0d9f48466f00ca78154f4
Melnyk_Volodymyr <olgreen123@gmail.com>
2020-11-16 17:48:21 +0200

    HW_2 taskTwo

--- src/HomeWork_2/taskOne.java
+++ src/HomeWork_2/taskOne.java
@@ -1,11 +1,25 @@
 package HomeWork_2;
 
+import java.util.Scanner;
+
 public class taskOne {
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Рахуємо, яким буде вклад за певну кількість років, \n" +
+                "якщо ви введете суму та процентну ставку.\n" +
+                "");
+        System.out.print("Введіть суму грн (лише цифри) : ");
+        double m = scan.nextDouble();
+        System.out.print("Введіть процентну ставку (% річних (лише цифри)) : ");
+        float n = scan.nextFloat();
+        System.out.print("Термін - введіть кількість років (лише ифри) : ");
+        float k = scan.nextFloat();
 
-
-
+        Double result = m + (m * n / 100 * k);
+        System.out.println("\nЯкщо покласти суму " + m + "грн, під " +
+                n + " % на " + k + " років \n" +
+                "Можна отримати : " + result + " грн");
     }
 }
 
--- src/HomeWork_2/taskTwo.java
+++ src/HomeWork_2/taskTwo.java
@@ -1,4 +1,13 @@
 package HomeWork_2;
 
+import java.util.Scanner;
+
 public class taskTwo {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String[] array = new String[10];
+    }
 }
+//    Користувач задає елементи масиву певного розміру (розмір масиву вибирає програміст).
+//    Ваше завдання вивести масив, який ввів користувач, а потім вивести масив у зворотньому порядку.
