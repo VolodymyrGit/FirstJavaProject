2c77e111a8cd9dfa95e8813cf0e0379acba0464b
Melnyk_Volodymyr <olgreen123@gmail.com>
2020-12-11 18:09:56 +0200

    HW_5 Done

--- src/home_Work_5/ConsoleMenu.java
+++ src/home_Work_5/ConsoleMenu.java
@@ -10,48 +10,58 @@ public class ConsoleMenu {
         String yes_No = "yes";
 
         while (!yes_No.equalsIgnoreCase("no")) {
-            System.out.println("Enter the name of the month on English ");
-            String str = scanner.nextLine();
 
-            while (Months.checkIfThereIsSuchAMonth(str) == 0)
+            if (yes_No.equalsIgnoreCase("yes")) {
 
+                while (yes_No.equalsIgnoreCase("yes")) {
+                    System.out.println("Enter the name of the month on English ");
+                    String str = scanner.nextLine();
+                    System.out.println();
 
-                Months.checkIfThereIsSuchAMonth(str);
-            System.out.println(Months.checkIfThereIsSuchAMonth(str));
-            Months.MARCH.displayAllMonthsWithTheSameSeason().
-                    displayAllMonthsWithTheSameNumberOfDays().
-                    displayAllMonthsThatHaveFewerDays().
-                    displayAllMonthsThatHaveMoreDays().
-                    displayTheNextSeason().
-                    displayThePreviousSeason().
-                    displayAllMonthsThatHaveAnEvenNumberOfDays().
-                    displayAllMonthsThatHaveAnOddNumberOfDays();
-
-            if (!yes_No.equalsIgnoreCase("no")) {
-                System.out.println("You entered an incorrect value.");
+                    if (Months.checkIfThereIsSuchAMonth(str) != 0) {
+                        Months.valueOf(str.toUpperCase()).
+                                displayWhetherTheMonthEnteredFromTheConsoleHasAnEvenNumberOfDays().
+                                displayAllMonthsThatHaveAnEvenNumberOfDays().
+                                displayAllMonthsThatHaveAnOddNumberOfDays().
+                                displayAllMonthsWithTheSameNumberOfDays().
+                                displayAllMonthsThatHaveFewerDays().
+                                displayAllMonthsThatHaveMoreDays().
+                                displayAllMonthsWithTheSameSeason().
+                                displayTheNextSeason().
+                                displayThePreviousSeason();
+                    }
+                    System.out.println("Would you like to continue work with the months ( YES/NO )");
+                    yes_No = scanner.nextLine();
+                    System.out.println();
+                }
+            } else if (!yes_No.equalsIgnoreCase("yes")) {
+                System.out.println("You entered an incorrect value - " + yes_No + "\n" +
+                        "Would you like to continue work with the months ( YES/NO )");
+                yes_No = scanner.nextLine();
+                System.out.println();
             }
-            System.out.println("Would you like to continue work with the months ( YES/NO )");
-            yes_No = scanner.nextLine();
         }
         System.out.println("Goodbye");
     }
 
-    public Months readMonthFromScanner () {
-        System.out.println("Enter the name of the month on English ");
-        String s = scanner.nextLine();
-        for (Months m : Months.values()) {
-            if (!s.equalsIgnoreCase(m.name())) {
-                while (!s.equalsIgnoreCase(m.name())) {
-                    while (!s.equalsIgnoreCase("NO")) {
-                        System.out.println("You entered an incorrect value.\n" +
-                                "Would you like to continue work with the months ( YES/NO )");
-                        s = scanner.nextLine();
-                    }
-                    System.out.println("Enter the name of the month on English ");
-                }
-            }
-        }
-        return Months.valueOf(s);
-    }
-
 }
+//    Написати консольну програму для роботи з Enums.
+//    Створити енум Сезони, в якому оголосити такі константи : Зима , Весна, Літо, Осінь.
+//    Створити енум Місяці, в якому створити 12 констант- місяці року(Січень , Лютий.. Грудень ),
+//    оголосити змінну дні, та змінну сезон типу Сезон , як поле енума .
+//    Створити конструктор з визначеними параметрами в енумі Місяці,
+//    в який як параметри передати змінну дні та сезон.
+//    Описати getters до даних полів (дні, сезони).
+//    Створити консольне меню, в якому реалізувати наступні пункти :
+//
+//    Перевірити чи є такий місяць (місяць вводимо з консолі, передбачити, щоб регістр
+//    букв був не важливим ).
+//    Вивести всі місяці з такою ж порою року.
+//    Вивести всі місяці які мають таку саму кількість днів.
+//    Вивести на екран всі місяці які мають меншу кількість днів.
+//    Вивести на екран всі місяці які мають більшу кількість днів.
+//    Вивести на екран наступну пору року.
+//    Вивести на екран попередню пору року.
+//    Вивести на екран всі місяці які мають парну кількість днів.
+//    Вивести на екран всі місяці які мають непарну кількість днів.
+//    Вивести на екран чи введений з консолі місяць має парну кількість днів.
--- src/home_Work_5/Months.java
+++ src/home_Work_5/Months.java
@@ -22,33 +22,48 @@ public enum Months {
         this.season = season;
     }
 
+
     public int getNumberOfDays() {
         return numberOfDays;
     }
+
 
     public Seasons getSeason() {
         return season;
     }
 
-    public static int checkIfThereIsSuchAMonth (String string) {
+
+    public static int checkIfThereIsSuchAMonth(String string) {
         int i = 0;
-        for(Months m : Months.values()) {
-            if(string.toUpperCase().equals(m.name())) {
+        for (Months m : Months.values()) {
+            if (string.toUpperCase().equals(m.name())) {
                 i = 1;
                 System.out.println("There is such a month of the year - " + m.name());
             }
         }
-        if (i == 0){
-            System.out.println("There is no such month in the year");
+        if (i == 0) {
+            System.out.println("There is no such month in the year - " + string);
         }
         System.out.println();
         return i;
     }
 
-    public Months displayAllMonthsWithTheSameSeason () {
-        for(Months m : Months.values()) {
-            if(season == m.season && !name().equals(m.name())) {
-                System.out.println(m + "- This month is from the same season - " + m.season);
+
+    public Months displayWhetherTheMonthEnteredFromTheConsoleHasAnEvenNumberOfDays() {
+        if (numberOfDays % 2 == 0) {
+            System.out.println(name() + " - This month has an even number of days - " + numberOfDays);
+        } else {
+            System.out.println(name() + " - This month has an odd number of days - " + numberOfDays);
+        }
+        System.out.println();
+        return valueOf(name());
+    }
+
+
+    public Months displayAllMonthsThatHaveAnEvenNumberOfDays() {
+        for (Months m : Months.values()) {
+            if (m.numberOfDays % 2 == 0) {
+                System.out.println(m.name() + " - This month has an even number of days - " + m.numberOfDays);
             }
         }
         System.out.println();
@@ -56,9 +71,20 @@ public enum Months {
     }
 
 
-    public Months displayAllMonthsWithTheSameNumberOfDays () {
-        for(Months m : Months.values()) {
-            if(numberOfDays == m.numberOfDays && !name().equals(m.name())) {
+    public Months displayAllMonthsThatHaveAnOddNumberOfDays() {
+        for (Months m : Months.values()) {
+            if (m.numberOfDays % 2 != 0) {
+                System.out.println(m.name() + " - This month has an odd number of days - " + m.numberOfDays);
+            }
+        }
+        System.out.println();
+        return valueOf(name());
+    }
+
+
+    public Months displayAllMonthsWithTheSameNumberOfDays() {
+        for (Months m : Months.values()) {
+            if (numberOfDays == m.numberOfDays && !name().equals(m.name())) {
                 System.out.println(m.name() + "- Month with the same number of days - " + m.numberOfDays);
             }
         }
@@ -66,9 +92,10 @@ public enum Months {
         return valueOf(name());
     }
 
-    public Months displayAllMonthsThatHaveFewerDays () {
-        for(Months m : Months.values()) {
-            if(numberOfDays > m.numberOfDays && !name().equals(m.name())) {
+
+    public Months displayAllMonthsThatHaveFewerDays() {
+        for (Months m : Months.values()) {
+            if (numberOfDays > m.numberOfDays && !name().equals(m.name())) {
                 System.out.println(m.name() + "- This month has fewer days - " + m.numberOfDays);
             }
         }
@@ -79,9 +106,10 @@ public enum Months {
         return valueOf(name());
     }
 
-    public Months displayAllMonthsThatHaveMoreDays () {
-        for(Months m : Months.values()) {
-            if(numberOfDays < m.numberOfDays && !name().equals(m.name())) {
+
+    public Months displayAllMonthsThatHaveMoreDays() {
+        for (Months m : Months.values()) {
+            if (numberOfDays < m.numberOfDays && !name().equals(m.name())) {
                 System.out.println(m.name() + "- This month has more days - " + m.numberOfDays);
             }
         }
@@ -92,7 +120,19 @@ public enum Months {
         return valueOf(name());
     }
 
-    public Months displayTheNextSeason () {
+
+    public Months displayAllMonthsWithTheSameSeason() {
+        for (Months m : Months.values()) {
+            if (season == m.season && !name().equals(m.name())) {
+                System.out.println(m + "- This month is from the same season - " + m.season);
+            }
+        }
+        System.out.println();
+        return valueOf(name());
+    }
+
+
+    public Months displayTheNextSeason() {
         Seasons[] seasons = Seasons.values();
         for (int i = 0; i < seasons.length; i++) {
             if (season.equals(seasons[i]) && i != 3) {
@@ -105,7 +145,8 @@ public enum Months {
         return valueOf(name());
     }
 
-    public Months displayThePreviousSeason () {
+
+    public Months displayThePreviousSeason() {
         Seasons[] seasons = Seasons.values();
         for (int i = 0; i < seasons.length; i++) {
             if (season.equals(seasons[i]) && i != 0) {
@@ -118,23 +159,6 @@ public enum Months {
         return valueOf(name());
     }
 
-    public Months displayAllMonthsThatHaveAnEvenNumberOfDays () {
-        for(Months m : Months.values()) {
-            if(m.numberOfDays % 2 == 0) {
-                System.out.println(m.name() + " - This month has an even number of days - " + m.numberOfDays);
-            }
-        }
-        System.out.println();
-        return valueOf(name());
-    }
-
-    public Months displayAllMonthsThatHaveAnOddNumberOfDays () {
-        for(Months m : Months.values()) {
-            if(m.numberOfDays % 2 != 0) {
-                System.out.println(m.name() + " - This month has an odd number of days - " + m.numberOfDays);
-            }
-        }
-        System.out.println();
-        return valueOf(name());
-    }
 }
+
+
