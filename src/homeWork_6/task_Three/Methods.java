b557c912186976833f5edec409a790c1b3333b59
Melnyk_Volodymyr <olgreen123@gmail.com>
2020-12-18 17:00:30 +0200

    HW_6 Corrections

--- src/homeWork_6/task_Three/Methods.java
+++ src/homeWork_6/task_Three/Methods.java
@@ -6,23 +6,20 @@ public class Methods {
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-
         System.out.println("Попрацюємо? (YES/NO)");
-        while (continue$()) {
+
+        while (doYouWantToContinue()) {
             System.out.println("Введіть змінну а");
             double a = scanner.nextDouble();
             System.out.println("Введіть змінну b");
             double b = scanner.nextDouble();
 
-            validateVariables(a,b);
-
-            boolean valid = validateVariables(a,b);
-
-            if (valid) {
+            if (validateVariables(a,b)) {
                 adding(a, b);
                 subtraction(a, b);
                 multiplication(a, b);
                 division(a, b);
+                System.out.println();
             }
             System.out.println("Бажаєте продовжити? (YES/NO)");
             String s = scanner.nextLine();
@@ -31,32 +28,32 @@ public class Methods {
 
 
     public static boolean validateVariables(double a, double b) {
-        boolean valid = true;
+        boolean valid = false;
         try {
             VariablesValidator.validate(a, b);
+            valid = true;
         } catch (IllegalArgumentException e) {
             System.out.println("Помилка : a < 0 і b < 0");
-            valid = false;
         } catch (ArithmeticException e) {
             System.out.println("Помилка : a = 0 і b != 0 або a != 0 і b = 0");
-            valid = false;
         } catch (IllegalAccessException e) {
             System.out.println("Помилка : a = 0 i b = 0");
-            valid = false;
         } catch (MyException e) {
             System.out.println(e.getMessage());
-            valid = false;
         }
+        System.out.println();
         return valid;
     }
 
 
-    public static boolean continue$() {
+    public static boolean doYouWantToContinue() {
         String s = scanner.nextLine();
+        System.out.println();
         boolean yes = true;
         while (!s.equalsIgnoreCase("no") && !s.equalsIgnoreCase("yes")) {
             System.out.println("Ви ввели помилкове значення.\nБажаєте продовжити? (YES/NO)");
             s = scanner.nextLine();
+            System.out.println();
         }
         if (s.equalsIgnoreCase("no")) {
             yes = false;
