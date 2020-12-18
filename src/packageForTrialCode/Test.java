0c23350d0d568386601a2aa3ca13458c9715eca5
Melnyk_Volodymyr <olgreen123@gmail.com>
2020-12-18 16:20:24 +0200

    Code samples

---
+++ src/packageForTrialCode/Test.java
@@ -0,0 +1,14 @@
+package packageForTrialCode;
+
+import java.util.Arrays;
+
+public class Test {
+
+    public static void main(String[] args) {
+        String s = "text sfs sfs sfs";
+//        String[] splitted = s.split(" ");
+        for (String st : s.split(" ")) {
+            System.out.println(st);
+        }
+    }
+}
